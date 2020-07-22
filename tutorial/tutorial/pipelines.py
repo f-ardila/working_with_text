beb973079cc6a5c688714b17eccc610daacd99dc
f-ardila <fardila@ucsc.edu>
2020-07-22 12:13:56 -0700

    scrapy tutorial to learn to use scrapy with their project

---
+++ tutorial/tutorial/pipelines.py
@@ -0,0 +1,23 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+from scrapy.exporters import JsonItemExporter
+
+class TutorialPipeline:
+        def __init__(self):
+            self.file = open("books.json", 'wb')
+            self.exporter = JsonItemExporter(self.file, encoding='utf-8', ensure_ascii=False)
+            self.exporter.start_exporting()
+
+        def close_spider(self, spider):
+            self.exporter.finish_exporting()
+            self.file.close()
+
+        def process_item(self, item, spider):
+            self.exporter.export_item(item)
+            return item
